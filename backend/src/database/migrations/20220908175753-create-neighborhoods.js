f1d71a648fc412cefc7e4d799707fdcd373fd1f1
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-09-09 11:46:47 -0300

    backend - migrations: Set create-neighborhoods migration.

---
+++ backend/src/database/migrations/20220908175753-create-neighborhoods.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('neighborhoods', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      cityId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'cities', key: 'id' },
+      },
+    });
+  },
+
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.dropTable('neighborhoods');
+  }
+};
