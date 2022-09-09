636631e2d20a673ae89aad9459844e2a30a7b86f
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-09-09 11:46:30 -0300

    backend - migrations: Set create-telephones migration.

---
+++ backend/src/database/migrations/20220908175521-create-telephones.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('telephones', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      prefix: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      number: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      cityId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'cities', key: 'id' },
+      },
+    });
+  },
+
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.dropTable('telephones');
+  }
+};
