1398461d9d2baabd19359e2638ee3d9296e1bb22
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-09-09 11:41:28 -0300

    backend - models: Set Telephone model.

---
+++ backend/src/database/models/Telephone.model.ts
@@ -0,0 +1,47 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+import City from './City.model';
+
+class Telephone extends Model {
+  public id!: number;
+
+  public prefix!: number;
+
+  public number!: number;
+
+  public cityId!: number;
+}
+
+Telephone.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    prefix: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    number: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    cityId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    timestamps: false,
+    modelName: 'telephne',
+    tableName: 'telephones',
+  },
+);
+
+Telephone.belongsTo(City, { foreignKey: 'cityId', as: 'city' });
+
+export default Telephone;
