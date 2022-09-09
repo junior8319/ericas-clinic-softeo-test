02cf5d016e4fda6ae9c1ad19cf302e2d98ee6bc7
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-09-09 11:42:17 -0300

    backend - models: Set PublicPlace model.

---
+++ backend/src/database/models/PublicPlace.model.ts
@@ -0,0 +1,47 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+import Neighborhood from './Neighborhood.model';
+
+class PublicPlace extends Model {
+  public id!: number;
+
+  public type!: string;
+
+  public name!: string;
+
+  public neighborhoodId!: number;
+}
+
+PublicPlace.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    neighborhoodId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: db,
+    underscored: true,
+    timestamps: false,
+    modelName: 'publicPlace',
+    tableName: 'pulic_places',
+  }
+);
+
+PublicPlace.belongsTo(Neighborhood, { foreignKey: 'neighborhoodId', as: 'neighborhood' });
+
+export default PublicPlace;
