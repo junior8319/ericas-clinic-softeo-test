7b3b4128878c40336981e2f0616ef98db9ba581f
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-09-13 11:04:59 -0300

    backend - transpilation result: javascript server.js generated by correction in server.ts.

--- backend/build/server/server.js
+++ backend/build/server/server.js
@@ -1,6 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
+const app_1 = __importDefault(require("./app"));
 require("dotenv/config");
-const PORT = process.env.PORT || 3001;
-new app_1.App().start(PORT);
+const PORT = process.env.PORT || 3005;
+console.log(PORT);
+app_1.default.listen(Number(PORT));
