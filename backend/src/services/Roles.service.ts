8e2148c51809e4797e45276b75fc6b515666bfd5
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-09-18 14:50:31 -0300

    backend - services - Roles: Sets createRole method.

--- backend/src/services/Roles.service.ts
+++ backend/src/services/Roles.service.ts
@@ -4,6 +4,8 @@ import IRole from "../interfaces/role.interface";
 class Roles {
   static model: Role;
 
+  public name!: string;
+
   constructor() {
     Roles.model = new Role();
   }
@@ -14,6 +16,26 @@ class Roles {
     if (!roles) return null;
 
     return roles;
+  };
+
+  static roleExists = async (receivedName: string): Promise<boolean> => {
+    const role = await Role.findOne({
+      where: { name: receivedName },
+    });
+
+    const exists = !!role;
+
+    return exists;
+  };
+
+  public createRole = async (role: IRole): Promise<IRole | null> => {
+    if (!role) return null;
+
+    this.name = role.name;
+
+    const createdRole = await Role.create({ ...role });
+
+    return createdRole;
   };
 }
 
