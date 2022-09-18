ce7cfc8d2a70f5c99a3155a875ee47a42b76d398
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-09-18 14:52:25 -0300

    backend - routes - Roles: Sets route to create a role. verb: POST.

--- backend/src/routes/roles.routes.ts
+++ backend/src/routes/roles.routes.ts
@@ -4,5 +4,6 @@ import RolesController from "../controllers/Roles.controller";
 const rolesRouter = Router();
 
 rolesRouter.get('/roles', RolesController.getRoles);
+rolesRouter.post('/roles', RolesController.createRole);
 
 export default rolesRouter;
