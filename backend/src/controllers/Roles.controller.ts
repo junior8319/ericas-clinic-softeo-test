6be43d8fb2ea72350e0ad21790a71446f17c1cfd
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-09-18 14:50:58 -0300

    backend - controllers - Roles: Sets createRole method.

--- backend/src/controllers/Roles.controller.ts
+++ backend/src/controllers/Roles.controller.ts
@@ -21,6 +21,24 @@ class Roles {
       errorMiddleware.handleErrors();
     }
   };
+
+  public createRole = async (req: Request, res: Response, _next: NextFunction) => {
+    try {
+      const role = req.body;
+
+      const createdRole: IRole | null = await this.service.createRole(role);
+      
+      if (!createdRole) {
+        return res.status(400)
+          .json({ message: `Não foi possível criar a função com os dados: ${role}` });
+      }
+
+      return res.status(201).json(createdRole);
+    } catch (error) {
+      console.log(error);
+      return errorMiddleware.handleErrors();
+    }
+  };
 }
 
 export default new Roles();
