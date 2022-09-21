d4ac141d9c0679a8f1d819a7e891346126a22647
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-09-21 10:00:54 -0300

    backend - routes - Roles: Sets routes to update and delete a country. verbs: PUT and DELETE.

--- backend/src/routes/roles.routes.ts
+++ backend/src/routes/roles.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import RolesController from "../controllers/Roles.controller";
+import errorMiddleware from '../middlewares/error.middleware';
 
 const rolesRouter = Router();
 
-rolesRouter.get('/roles', RolesController.getRoles);
-rolesRouter.post('/roles', RolesController.createRole);
+rolesRouter.get('/roles', RolesController.getRoles, errorMiddleware.handleErrors);
+rolesRouter.post('/roles', RolesController.createRole, errorMiddleware.handleErrors);
+rolesRouter.put('/roles/:id', RolesController.updateRole, errorMiddleware.handleErrors);
+rolesRouter.delete('/roles/:id', RolesController.deleteRole, errorMiddleware.handleErrors);
 
 export default rolesRouter;
