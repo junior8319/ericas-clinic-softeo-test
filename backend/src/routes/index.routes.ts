2932f611c2421d56a17e13f669e398994232b02e
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-09-22 14:37:01 -0300

    backend - routes - index: Adds cities and neighborhoods routers to index.

--- backend/src/routes/index.routes.ts
+++ backend/src/routes/index.routes.ts
@@ -1,7 +1,11 @@
 import countriesRouter from './countries.routes';
 import rolesRouter from './roles.routes';
+import citiesRouter from './cities.routes';
+import neighborhoodsRouter from './neighborhoods.routes';
 
 export {
   countriesRouter,
   rolesRouter,
+  citiesRouter,
+  neighborhoodsRouter,
 };
