86f9ce4d438779b0e9d887ad0e8896dc4bd70373
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-09-22 16:17:39 -0300

    backend - routes - index: Adds telephones router to index.

--- backend/src/routes/index.routes.ts
+++ backend/src/routes/index.routes.ts
@@ -2,10 +2,12 @@ import countriesRouter from './countries.routes';
 import rolesRouter from './roles.routes';
 import citiesRouter from './cities.routes';
 import neighborhoodsRouter from './neighborhoods.routes';
+import telephonesRouter from './telephones.routes';
 
 export {
   countriesRouter,
   rolesRouter,
   citiesRouter,
   neighborhoodsRouter,
+  telephonesRouter,
 };
