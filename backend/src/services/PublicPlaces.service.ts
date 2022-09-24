34df2662cd8aa9b042c2e2e1df80f1e25d94cd18
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-09-24 11:11:59 -0300

    backend - services - PublicPlaces: Sets CRUD methods for public places service.

---
+++ backend/src/services/PublicPlaces.service.ts
@@ -0,0 +1,93 @@
+import Neighborhood from '../database/models/Neighborhood.model';
+import PublicPlace from '../database/models/PublicPlace.model';
+import IPublicPlace from '../interfaces/publicPlace.interface';
+
+class PublicPlaces {
+  static model: PublicPlace;
+
+  public name!: string;
+
+  public id!: number;
+
+  constructor() {
+    PublicPlaces.model = new PublicPlace();
+  }
+
+  public getPublicPlaces = async (): Promise<IPublicPlace[] | null> => {
+    const publicPlacesList = await PublicPlace.findAll({
+      raw: true,
+      include: [
+        { model: Neighborhood, as: 'neighborhood', attributes: { exclude: ['id'] } },
+      ],
+    });
+    if (!publicPlacesList) return null;
+
+    return publicPlacesList;
+  };
+
+  static publicPlaceExists = async (receivedName: string): Promise<boolean> => {
+    const publicPlace = await PublicPlace.findOne({
+      where: { name: receivedName },
+    });
+
+    const exists = !!publicPlace;
+
+    return exists;
+  };
+
+  public createPublicPlace = async (publicPlace: IPublicPlace): Promise<IPublicPlace | null> => {
+    if (!publicPlace) return null;
+
+    this.name = publicPlace.name;
+    
+    const publicPlaceExists = await PublicPlaces.publicPlaceExists(this.name);
+    if (publicPlaceExists) return null;
+
+    const createdPublicPlace = await PublicPlace.create({ ...publicPlace });
+
+    return createdPublicPlace;
+  };
+
+  public updatePublicPlace = async (publicPlace: IPublicPlace): Promise<IPublicPlace | null> => {
+    if (!publicPlace) return null;
+
+    if (publicPlace.id) this.id = publicPlace.id;
+
+    const publicPlaceToUpdate = await PublicPlace.findOne({ where: { id: this.id }});
+    if (!publicPlaceToUpdate) return null;
+
+    if (publicPlace.name) {
+      await publicPlaceToUpdate.update({
+        name: publicPlace.name,
+      });
+    }
+
+    if (publicPlace.type) {
+      await publicPlaceToUpdate.update({
+        type: publicPlace.type,
+      });
+    }
+
+    if (publicPlace.neighborhoodId) {
+      await publicPlaceToUpdate.update({
+        neighborhoodId: publicPlace.neighborhoodId,
+      });
+    }
+
+    return publicPlaceToUpdate;
+  };
+
+  public deletePublicPlace = async (receivedId: string): Promise<IPublicPlace | null> => {
+    if (!receivedId) return null;
+
+    this.id = Number(receivedId);
+
+    const publicPlaceToDelete = await PublicPlace.findOne({ where: { id: this.id } });
+
+    if (publicPlaceToDelete) await publicPlaceToDelete.destroy();
+
+    return publicPlaceToDelete;
+  };
+}
+
+export default PublicPlaces;
