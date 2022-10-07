62989db8e1e137714e81937cccad605d5ec8698e
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-10-07 14:51:08 -0300

    feature - backend - routes - roles: Add validation middleware to roles router.

--- backend/src/routes/roles.routes.ts
+++ backend/src/routes/roles.routes.ts
@@ -1,12 +1,28 @@
 import { Router } from 'express';
 import RolesController from "../controllers/Roles.controller";
 import errorMiddleware from '../middlewares/error.middleware';
+import RolesMiddleware from '../middlewares/validateRoles.middleware';
 
 const rolesRouter = Router();
 
 rolesRouter.get('/roles', RolesController.getRoles, errorMiddleware.handleErrors);
-rolesRouter.post('/roles', RolesController.createRole, errorMiddleware.handleErrors);
-rolesRouter.put('/roles/:id', RolesController.updateRole, errorMiddleware.handleErrors);
-rolesRouter.delete('/roles/:id', RolesController.deleteRole, errorMiddleware.handleErrors);
+rolesRouter.post(
+  '/roles',
+  RolesMiddleware.validateCreateRoles,
+  RolesController.createRole,
+  errorMiddleware.handleErrors,
+);
+rolesRouter.put(
+  '/roles/:id',
+  RolesMiddleware.validateUpdateRole,
+  RolesController.updateRole,
+  errorMiddleware.handleErrors
+);
+rolesRouter.delete(
+  '/roles/:id',
+  RolesMiddleware.validateDeleteRole,
+  RolesController.deleteRole,
+  errorMiddleware.handleErrors
+);
 
 export default rolesRouter;
