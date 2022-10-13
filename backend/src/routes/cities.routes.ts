b047c52515f88840c654400ca5325f341581b5aa
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-10-13 07:42:52 -0300

    feature - backend - routes - cities: Add validation middleware to cities router.

--- backend/src/routes/cities.routes.ts
+++ backend/src/routes/cities.routes.ts
@@ -1,12 +1,36 @@
 import { Router } from 'express';
 import CitiesController from '../controllers/Cities.controller';
+import CitiesMiddleware from '../middlewares/validateCities.middleware';
 import errorMiddleware from '../middlewares/error.middleware';
 
 const citiesRouter = Router();
 
-citiesRouter.get('/cities', CitiesController.getCities, errorMiddleware.handleErrors);
-citiesRouter.post('/cities', CitiesController.createCity, errorMiddleware.handleErrors);
-citiesRouter.put('/cities/:id', CitiesController.updateCity, errorMiddleware.handleErrors);
-citiesRouter.delete('/cities/:id', CitiesController.deleteCity, errorMiddleware.handleErrors);
+citiesRouter.get(
+  '/cities',
+  CitiesController.getCities,
+  errorMiddleware.handleErrors
+);
+
+citiesRouter.post(
+  '/cities',
+  CitiesMiddleware.validateCreateCity,
+  CitiesController.createCity,
+  errorMiddleware.handleErrors
+);
+
+citiesRouter.put(
+  '/cities/:id',
+  CitiesMiddleware.validateUpdateCity,
+  CitiesController.updateCity,
+  errorMiddleware.handleErrors
+);
+
+citiesRouter.delete(
+  '/cities/:id',
+  CitiesMiddleware.validateDeleteCity,
+  CitiesController.deleteCity,
+  errorMiddleware.handleErrors
+);
+
 
 export default citiesRouter;
