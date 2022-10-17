3a7f0393e11c286a903fcfc460c014af5eac6ca2
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-10-17 08:12:41 -0300

    fixes/features - backend - middlewares - validateTelephones: Changes the validateCreatePhone method to: Fix the order of varification. Change to export a instantiated object of this class. Sets validateUpdatePhone and validateDeletePhone methods.

--- backend/src/middlewares/validateTelephones.middleware.ts
+++ backend/src/middlewares/validateTelephones.middleware.ts
@@ -20,15 +20,12 @@ class TelephonesMiddleware {
   public validateCreatePhone = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const phoneToCreate = req.body;
-      const { prefix, number, cityId } = phoneToCreate;
-      const phoneExists = await TelephonesService.telephoneExists(prefix, number, cityId);
-
-      if (phoneExists) return res.status(400)
-        .json({ message: `Já existe número de telefone com os dados: ${phoneToCreate}` });
-
-      if (!phoneToCreate) return res.status(400)
+      if (!phoneToCreate || Object.keys(phoneToCreate).length === 0) {
+        return res.status(400)
         .json({ message: 'Nenhum dado recebido.' });
-
+      }
+      
+      const { prefix, number, cityId } = phoneToCreate;
       this.prefix = phoneToCreate.prefix;
       if (!this.prefix || !Number(this.prefix)) return res.status(400)
         .json({ message: 'Prefixo do telefone não informado, informe "prefix"' });
@@ -41,6 +38,71 @@ class TelephonesMiddleware {
       if (!this.cityId || !Number(this.cityId)) return res.status(400)
         .json({ message: 'É necessário informar o id da cidade, informe "cityId"' });
 
+      const phoneExists = await TelephonesService.telephoneExists(prefix, number, cityId);
+      if (phoneExists) return res.status(400)
+        .json({ message: `Já existe número de telefone com os dados: ${phoneToCreate}` });
+  
+      next();
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  };
+
+  public validateUpdatePhone = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const phoneToUpdate = req.body;
+      console.log('PHONETOUPDATE', phoneToUpdate);
+      const { prefix, number, cityId } = phoneToUpdate;
+
+      if (!phoneToUpdate || !prefix && !number && !cityId) return res.status(400)
+        .json({ message: 'Sem dado para atualizar' });
+
+      const { id } = req.params;
+      if (!id || !Number(id)) return res.status(400)
+        .json({
+          message:
+            'Por favor, nos passe um identificador(id) numérico para atualizar.',
+        });
+
+      const foundPhone = await TelephonesService.getPhoneById(Number(id));
+      if (!foundPhone) return res.status(400)
+        .json({
+          message:
+          `Identificador informado (id: ${id}) não encontrado` +
+          ' Favor informar id válido',
+        });
+
+      if (!prefix || !number || !cityId) return next();
+      const phoneExists = await TelephonesService.telephoneExists(prefix, number, cityId);
+      if (phoneExists) return res.status(400)
+        .json({ message: `Já existe telefone com os dados ${phoneToUpdate}` });
+
+      next();
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  };
+
+  public validateDeletePhone = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params;
+
+      if (!id || !Number(id)) return res.status(400)
+        .json({
+          message:
+            'Por favor, nos passe um identificador (id) numérico para excluir.'
+        });
+      
+      const foundPhone = await TelephonesService.getPhoneById(Number(id));
+      if (!foundPhone) return res.status(400)
+        .json({
+          message:
+            `Identificador (id: ${id} não encontrado.` +
+            ' Favor informar id válido para excluir.',
+        });
+      
       next();
     } catch (error) {
       console.log(error);
@@ -49,4 +111,4 @@ class TelephonesMiddleware {
   };
 }
 
-export default TelephonesMiddleware;
+export default new TelephonesMiddleware();
