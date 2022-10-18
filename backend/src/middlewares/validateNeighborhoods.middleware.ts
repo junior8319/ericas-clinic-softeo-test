3818f8bd6392a95872175ee96c30797aeb006f8c
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-10-18 07:36:27 -0300

    features - backend - middlewares - validateNeighborhoods: Sets the method validateCreateNeighborhood.

---
+++ backend/src/middlewares/validateNeighborhoods.middleware.ts
@@ -0,0 +1,57 @@
+import express, { NextFunction, Request, Response } from 'express';
+import NeighborhoodsService from '../services/Neighborhoods.service';
+
+class NeighborhoodsMiddleware {
+  public app: express.Application;
+
+  public service: NeighborhoodsService;
+
+  public name!: string;
+
+  public cityId!: number;
+
+  constructor() {
+    this.app = express();
+    this.service = new NeighborhoodsService();
+  }
+
+  public validateCreateNeighborhood = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const neighborhoodToCreate = req.body;
+      if (
+        !neighborhoodToCreate ||
+        Object.keys(neighborhoodToCreate).length === 0
+      ) {
+        return res.status(400).json({ message: 'Nenhum dado recebido.' });
+      }
+
+      const { name, cityId } = neighborhoodToCreate;
+
+      this.name = name;
+      if (!this.name || this.name.length === 0) return res.status(400)
+        .json({ message: 'Você deve informar o nome do bairro ("name")' });
+
+      this.cityId = cityId;
+      if (!this.cityId || !Number(this.cityId)) return res.status(400)
+        .json({
+          message:
+            'Você deve informar a cidade onde se localiza ("cityId") o bairro.'
+        });
+
+      const neighborhoodExists = await NeighborhoodsService
+        .neighborhoodExists(this.name, this.cityId);
+      if (neighborhoodExists) return res.status(400)
+        .json({
+          message:
+            `Já existe bairro com o nome com os dados: ${neighborhoodToCreate}`
+        });
+
+      next();
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  };
+}
+
+export default new NeighborhoodsMiddleware();
