2b7b52e3b43f28a6793c68860cba007a77409aaf
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2022-11-13 09:07:34 -0300

    backend - helpers - testDates.helper.ts: Sets functions to verify if it is a valid date.

---
+++ backend/src/helpers/testDates.helper.ts
@@ -0,0 +1,61 @@
+const isLeapYear = (receivedYear: number): boolean => {
+  if (
+    receivedYear % 400 === 0 ||
+    receivedYear % 4 === 0 && !(receivedYear % 100 === 0)
+  ) return true;
+ 
+  return false;
+};
+
+const isInvalidMonthDay = (receivedMonth: number, receivedDay: number): string | false => {
+  const daysInMonths = {
+    months31: [1, 3, 5, 7, 8, 10, 12],
+    months30: [4, 6, 9, 11],
+  };
+  if (daysInMonths.months31.indexOf(receivedMonth) && receivedDay > 31) {
+    return `O mês ${receivedMonth} tem até 31 dias,`+
+      ` dia ${receivedDay} é uma data inválida`;
+  }
+  if (receivedDay > 30) {
+    return `O mês ${receivedMonth} tem até 30 dias,`+
+      ` dia ${receivedDay} é uma data inválida`;
+  }
+
+  return false;
+};
+
+const testDates = (receivedDate: string): string | false => {
+  try {
+    const year = receivedDate.split(/[/.-]/g)[0];
+    if (!year || year.length < 4) return 'Não foi informado um ano corretamente.';
+    const numberYear = Number(year);
+
+    const month = receivedDate.split(/[/.-]/g)[1];
+    if (!month || month.length < 2) {
+      return 'Não foi informado um mês corretamente.';
+    }
+    const numberMonth = Number(month);
+
+    if (!numberMonth || numberMonth <= 0 || numberMonth > 12) {
+      return 'Informe um número de 01 a 12 para o mês.';
+    }
+
+    const day = receivedDate.split(/[/.-]/g)[2];
+    const numberDay = Number(day);
+    if (!day || day.length < 2) return 'Não foi informado um dia corretamente.';
+    if (isLeapYear(numberYear) && numberMonth === 2 && numberDay > 29) {
+      return 'Para um ano bissexto, o mês de fevereiro tem apenas 29 dias.';
+    }
+    if (numberMonth === 2 && numberDay > 28 && !(isLeapYear(numberYear))) {
+      return 'Para ano não bissexto, o mês de fevereiro tem apenas 28 dias.';
+    }
+    if (isInvalidMonthDay(numberMonth, numberDay)) return isInvalidMonthDay(numberMonth, numberDay);
+
+    return false;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+export default testDates;
